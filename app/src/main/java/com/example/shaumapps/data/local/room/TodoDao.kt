3c52b33308c51669bc864b8ecafb0c65492491a6
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2023-02-23 14:29:49 +0700

    Setup Room, Retrofit, and Architecture Project

---
+++ app/src/main/java/com/example/shaumapps/data/local/room/TodoDao.kt
@@ -0,0 +1,20 @@
+package com.example.shaumapps.data.local.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.shaumapps.data.local.entity.Todo
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(todo: Todo)
+
+    @Update
+    fun update(todo: Todo)
+
+    @Delete
+    fun delete(todo: Todo)
+
+    @Query("SELECT * FROM todo ORDER BY id ASC")
+    fun getAllTodos(): LiveData<List<Todo>>
+}
