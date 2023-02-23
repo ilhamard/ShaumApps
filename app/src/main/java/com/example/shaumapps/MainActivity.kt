31c3f73c41ad035cd045874ec7b71a513fedcf8a
ilhaam1552 <ilham.ardiansyah@iconpln.co.id>
2023-02-23 14:35:00 +0700

    adding Kalender

--- app/src/main/java/com/example/shaumapps/MainActivity.kt
+++ app/src/main/java/com/example/shaumapps/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.shaumapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shaumapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnMasuk.setOnClickListener {
+            val intent = Intent(this@MainActivity, KalenderActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/shaumapps/kalender/Tanggal.kt
@@ -0,0 +1,7 @@
+package com.example.shaumapps.kalender
+
+
+data class Tanggal(
+    var tanggal: String,
+    var hari: String,
+)
